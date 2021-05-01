7654fa51954993910240943c853bf62a8194fa49
almogelias <57565283+almogelias@users.noreply.github.com>
2021-05-01 15:18:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 
 import tensorflow as tf
 import tensorflow_hub as hub
-
+from keras.layers.embeddings import Embedding
+from keras.preprocessing.text import Tokenizer
 import numpy as np
 import re
 import pretty_midi
@@ -10,90 +11,62 @@ import pandas as pd
 
 
 
-def wordList(lyrics_train_set):
-    word_list = {}
+def wordList(lyrics_train_set,word2vec_file):
+    word_dict = {}
+    word_list = []
     df = pd.read_csv(lyrics_train_set, header=None)
     df.loc[:, 2] = df[df.columns[2:]].apply(lambda x: ','.join(x.dropna().astype(str)), axis=1)
     df.drop(df.iloc[:, 3:], inplace=True, axis=1)
     for idx, line in enumerate(df.loc[:][2]):
         string_no_punctuation = re.sub("[^\w\s]", "", line)
-        word_list[idx] = string_no_punctuation.split()
-    return word_list
+        word_dict[idx] = string_no_punctuation.lower()
+        word_list.append(word_dict[idx])
 
+    tokenizer = Tokenizer()
+    tokenizer.fit_on_texts(word_list)
+    words_to_index = tokenizer.word_index
 
-def get_max_length(df):
-    """
-    get max token counts from train data,
-    so we use this number as fixed length input to RNN cell
-    """
-    max_length = 0
-    for row in df['review']:
-        if len(row.split(" ")) > max_length:
-            max_length = len(row.split(" "))
-    return max_length
+    word_to_vec_map = read_glove_vector(word2vec_file)
+    maxLen = 300
 
+    vocab_len = len(words_to_index)
+    embed_vector_len = word_to_vec_map['test'].shape[0]
 
-def get_word2vec_enc(reviews):
-    """
-    get word2vec value for each word in sentence.
-    concatenate word in numpy array, so we can use it as RNN input
-    """
-    encoded_reviews = []
-    for review in reviews:
-        tokens = review.split(" ")
-        word2vec_embedding = embed(tokens)
-        encoded_reviews.append(word2vec_embedding)
-    return encoded_reviews
+    emb_matrix = np.zeros((vocab_len, embed_vector_len))
 
+    for word, index in words_to_index.items():
+        embedding_vector = word_to_vec_map.get(word)
+        if embedding_vector is not None:
+            emb_matrix[index-1, :] = embedding_vector
 
-def get_padded_encoded_reviews(encoded_reviews):
-    """
-    for short sentences, we prepend zero padding so all input to RNN has same length
-    """
-    padded_reviews_encoding = []
-    for enc_review in encoded_reviews:
-        zero_padding_cnt = max_length - enc_review.shape[0]
-        pad = np.zeros((1, 250))
-        for i in range(zero_padding_cnt):
-            enc_review = np.concatenate((pad, enc_review), axis=0)
-        padded_reviews_encoding.append(enc_review)
-    return padded_reviews_encoding
+    embedding_layer = Embedding(input_dim=vocab_len, output_dim=embed_vector_len, input_length=maxLen,
+                                weights=[emb_matrix], trainable=False)
 
+    return embedding_layer
 
-def sentiment_encode(sentiment):
-    """
-    return one hot encoding for Y value
-    """
-    if sentiment == 'positive':
-        return [1, 0]
-    else:
-        return [0, 1]
+def read_glove_vector(glove_vec):
+  with open(glove_vec, 'r', encoding='UTF-8') as f:
+    words = set()
+    word_to_vec_map = {}
+    for line in f:
+      w_line = line.split()
+      curr_word = w_line[0]
+      word_to_vec_map[curr_word] = np.array(w_line[1:], dtype=np.float64)
 
-
-def preprocess(df):
-    """
-    encode text value to numeric value
-    """
-    # encode words into word2vec
-    reviews = df['review'].tolist()
-
-    encoded_reviews = get_word2vec_enc(reviews)
-    padded_encoded_reviews = get_padded_encoded_reviews(encoded_reviews)
-    # encoded sentiment
-    sentiments = df['sentiment'].tolist()
-    encoded_sentiment = [sentiment_encode(sentiment) for sentiment in sentiments]
-    X = np.array(padded_encoded_reviews)
-    Y = np.array(encoded_sentiment)
-    return X, Y
+  return word_to_vec_map
 
 if __name__ == '__main__':
 
-    lyrics_train_set = r'C:\Users\xadmin\Downloads\Assignment 3\Assignment 3\lyrics_train_set.csv'
-    word_list=wordList(lyrics_train_set)
+    lyrics_train_set = r'C:\Users\Anon\Downloads\Assignment 3\lyrics_train_set.csv'
+    word2vec_file=r'C:\Users\Anon\Downloads\Assignment 3\glove.6B.300d.txt'
+    embedding_layer=wordList(lyrics_train_set,word2vec_file)
 
+    midi_pretty_format = pretty_midi.PrettyMIDI(r'C:\Users\Anon\Downloads\Assignment 3\midi_files\adele_-_someone_like_you.mid')
+    piano_midi = midi_pretty_format.instruments[0]  # Get the piano channels
+    piano_roll = piano_midi.get_piano_roll(fs=30)
     # Load Pretrained Word2Vec
     #embed = hub.load("https://tfhub.dev/google/Wiki-words-250/2")
-    hub_url = "https://tfhub.dev/google/nnlm-en-dim128/2"
+    hub_url = r"C:\Users\Anon\Downloads\Assignment 3\Wiki-words-250_2"
     embed = hub.KerasLayer(hub_url)
     embeddings = embed(["A long sentence.", "single-word", "http://example.com"])
     embed(["friend"])
